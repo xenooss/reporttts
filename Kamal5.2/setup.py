753d2d868aa715453aada9589e73da593fa8bc5b
xenooss <101370790+xenooss@users.noreply.github.com>
2025-09-17 09:01:48 -1100

    Add files via upload

---
+++ Kamal5.2/setup.py
@@ -0,0 +1,11 @@
+# ENCODED BY : Kamal
+# ENCRYPTION : MARSHAL+ZLIB+B64
+# Telegram : https://t.me/Kamal939
+# Telegram Channel : https://t.me/+eTIBrGwcPjk3NzJl
+# ----------------------------------------------
+
+import marshal, zlib, base64
+
+# Your original script code starts here:
+# ----------------------------------------------
+exec(marshal.loads(zlib.decompress(base64.b64decode(b'eJy1VMluE0EQzYWLJf9DkRwmAWasCCRQpBwAc4gU2SjkRBRF7ZmaSZPe6CXJKOJ/+D9+gOrZPA4OEgd8aLlreVXzXnX9+vlsZ2cP0hcp5LrgqjqC4Mv0XbRMJ9MJl0ZbD9oNf41gvtRWRq+xXPn93YuXl3CinGdCEAJY/B64RYnKO8iybPcgxhZ4y3OE4wEgc7XzKPcb7x7MseQKN5Knkw2oY7iYToB+iUcrcy20TV51lubGJBsMMROdd4PBV7mtjdeDwaEPdNfCjVBUySvDrMM1tKmtrgh7jYQC/bVWg6FCTeSNUixThZZXgWCuWEXNr8EoD1WQK7TrsitkwfMyCKeDeTOYS3YzwkTJuNhsl3FdcoFukwUs4v2yZfVUVzwHohukXlEs7J9yFe5n58RguCfqeQm9MseQNM4EqH1I3i/mZ8uT+dXZcnmeAFc0BBmqW261OmrrtSeZOyUTZjwJKPUtgqkjRZDWycHWsGAK5vFJJxFeYJfdhuz1IwYKc3SO2RpWgYsCGlK24vAuIxeM5jKXRCgorr4xsMF5aO5Pdtgn+2BTi0Y/0c3n9kNfZ4eHf4VpCYlh20sabhpnH5+mPQvkgbtrRDEu/9Ei8QeulistSGJ6eTeuEbor1IjYqZBrKen6J0mCFHIlzEgL1h4UmvlmOGbNbM2Cs7MVV7NR//8QvuVD/3fNvmR/onDYDaxhzrXv4pF6huU39FDJGRkcbZ049+MldPT4c8oo3CDawyj2RyvXaEd+iRsnimFogeDzuBdH3oW+g1oHyBmVDAo+zN8efl0uPjXz3QT/BnItv9s='))))
